16becb55b57e08d5e3fbf442d17b15fd1231302c
james sewell <jamesjosephsewell@gmail.com>
2018-03-26 00:35:00 -0500

    .

--- backend/api_features/example_feature/itemSchema.js
+++ backend/api_features/example_feature/itemSchema.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose")
 // mongoose schema class
 const Schema = mongoose.Schema
 
-// instance of mongoose Schema cass aka mongoose model
+// instance of mongoose Schema class 
 var Item = new Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true }
-})
+    description: { type: String, required: true },
+}, { collection: 'items' })
 
 // exports mongoose model to the itemController.js file
 module.exports = mongoose.model("Item", Item)
